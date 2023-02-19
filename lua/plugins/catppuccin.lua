7b595d0c7238d3e6fe574784db375e865bfcd073
nixith <rwstauffer72@gmail.com>
2023-02-19 14:22:55 -0500

    add catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,12 +1,32 @@
+if true then return {} end
+-- Above disables the module
+
 return {
+
   -- add gruvbox
-  { "catppuccin/nvim", name = "catppuccin" },
+  { "catppuccin/nvim", 
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin"
+    end,
+    opts = {
+      flavour = "mocha",
+      background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = false,
+      flavour = "mocha",
+    }
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-mocha",
     },
   }
 }
